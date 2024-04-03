ed8a6ebb37a5375586833f47fc6dbf2fb774eb50
Piotr Frontczak <piotrf2023@gmail.com>
2024-04-03 10:55:15 +0200

    lightbox updated

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+
+
+
+for (const item of galleryItems) {
+  const html = `<a class="gallery__item" href="${item.original}">
+      <img class="gallery__image"
+        src="${item.preview}"
+        alt="${item.description}"
+      />
+    </a>`;
+
 console.log(galleryItems);
